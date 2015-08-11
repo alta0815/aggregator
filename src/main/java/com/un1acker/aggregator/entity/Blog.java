c27a1afea66547bae33859bf452d6d5c933c6492
un1acker <un1acker@yandex.ru>
2015-08-11 10:14:14 -0700

    add entities

---
+++ src/main/java/com/un1acker/aggregator/entity/Blog.java
@@ -0,0 +1,65 @@
+package com.un1acker.aggregator.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by un1acker on 8/7/2015.
+ */
+@Entity
+public class Blog {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String url;
+
+    private String name;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "blog")
+    private List<Item> items;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/un1acker/aggregator/entity/Item.java
@@ -0,0 +1,76 @@
+package com.un1acker.aggregator.entity;
+
+import javax.persistence.*;
+import javax.xml.ws.soap.MTOM;
+import java.util.Date;
+
+/**
+ * Created by un1acker on 8/7/2015.
+ */
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String title;
+
+    private String description;
+
+    @Column(name = "published_date")
+    private Date publishedDate;
+
+    private String link;
+
+    @ManyToOne
+    @JoinColumn(name = "blog_id")
+    private Blog blog;
+
+    public Blog getBlog() {
+        return blog;
+    }
+
+    public void setBlog(Blog blog) {
+        this.blog = blog;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
