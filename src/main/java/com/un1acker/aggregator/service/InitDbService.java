b80e76f94ee837c3045acc7fa255688d4f7282d7
un1acker <un1acker@yandex.ru>
2015-08-19 13:06:52 +0300

    add production db postgres and settings xml

--- src/main/java/com/un1acker/aggregator/service/InitDbService.java
+++ src/main/java/com/un1acker/aggregator/service/InitDbService.java
@@ -1,11 +1,9 @@
 package com.un1acker.aggregator.service;
 
 import com.un1acker.aggregator.entity.Blog;
-import com.un1acker.aggregator.entity.Item;
 import com.un1acker.aggregator.entity.Role;
 import com.un1acker.aggregator.entity.User;
 import com.un1acker.aggregator.repository.BlogRepository;
-import com.un1acker.aggregator.repository.ItemRepository;
 import com.un1acker.aggregator.repository.RoleRepository;
 import com.un1acker.aggregator.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,6 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Transactional
@@ -30,34 +27,34 @@ public class InitDbService {
     @Autowired
     private RoleRepository roleRepository;
 
-    @Autowired
-    private ItemRepository itemRepository;
-
     @PostConstruct
     public void init() {
-        Role roleUser = new Role();
-        roleUser.setName("ROLE_USER");
-        roleRepository.save(roleUser);
+        if (roleRepository.findByName("ROLE_ADMIN") == null) {
 
-        Role roleAdmin = new Role();
-        roleAdmin.setName("ROLE_ADMIN");
-        roleRepository.save(roleAdmin);
+            Role roleUser = new Role();
+            roleUser.setName("ROLE_USER");
+            roleRepository.save(roleUser);
 
-        User userAdmin = new User();
-        userAdmin.setName("admin");
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        userAdmin.setPassword(encoder.encode("admin"));
-        userAdmin.setEnabled(true);
-        List<Role> roles = new ArrayList<>();
-        roles.add(roleAdmin);
-        roles.add(roleUser);
-        userAdmin.setRoles(roles);
-        userRepository.save(userAdmin);
+            Role roleAdmin = new Role();
+            roleAdmin.setName("ROLE_ADMIN");
+            roleRepository.save(roleAdmin);
 
-        Blog blog = new Blog();
-        blog.setName("MyBlog");
-        blog.setUrl("http://feeds.feedburner.com/programs4you");
-        blog.setUser(userAdmin);
-        blogRepository.save(blog);
+            User userAdmin = new User();
+            userAdmin.setName("admin");
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            userAdmin.setPassword(encoder.encode("admin"));
+            userAdmin.setEnabled(true);
+            List<Role> roles = new ArrayList<>();
+            roles.add(roleAdmin);
+            roles.add(roleUser);
+            userAdmin.setRoles(roles);
+            userRepository.save(userAdmin);
+
+            Blog blog = new Blog();
+            blog.setName("MyBlog");
+            blog.setUrl("http://feeds.feedburner.com/programs4you");
+            blog.setUser(userAdmin);
+            blogRepository.save(blog);
+        }
     }
 }
